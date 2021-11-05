2c1dc9510b7922915c2b7f5f2b107cc264c68fd5
Lucas Kreber <kreberl@uni-trier.de>
2021-11-05 14:29:30 +0100

    added branch merge-conflict

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,10 @@ public class Main {
     public static void main(String[] args) {
         Factory factory = new Factory();
 
-        Car car = new Car("red", "BMW", 5);
+        Car car2 = new Car("red", "BMW", 5);
         Car car1 = new Car("blue", "Audi", 3);
 
-        factory.buildCar(car);
+        factory.buildCar(car2);
         factory.buildCar(car1);
     }
 }
