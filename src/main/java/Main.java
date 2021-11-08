9d4c06ef292e9301be9c621c05a5a613fcc59890
Lucas Kreber <kreberl@uni-trier.de>
2021-11-08 16:23:50 +0100

    Main.java updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,11 @@ public class Main {
     public static void main(String[] args) {
         Factory factory = new Factory();
 
-        Car car = new Car("red", "BMW", 5);
-        Car car1 = new Car("blue", "Audi", 3);
+        Car car1 = new Car("red", "BMW", 5);
+        Car car2 = new Car("blue", "Audi", 3);
+        Car car3 = new Car("blue", "VW", 3);
 
-        factory.buildCar(car);
         factory.buildCar(car1);
+        factory.buildCar(car2);
     }
 }
