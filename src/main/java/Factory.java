528c43319f040b39a418d20ed7407be08663f492
Lucas Kreber <kreberl@uni-trier.de>
2022-04-26 09:49:40 +0200

    Änderungen

--- src/main/java/Factory.java
+++ src/main/java/Factory.java
@@ -1,6 +1,6 @@
 public class Factory
 {
     public void buildCar(Car car) {
-        System.out.println("Build new car: color=" + car.getColor() + ", manufacturer=" + car.getManufacturer() + ", doors=" + car.getDoors());
+        //System.out.println("Build new car: color=" + car.getColor() + ", manufacturer=" + car.getManufacturer() + ", doors=" + car.getDoors());
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,6 @@ public class Main {
 
         factory.buildCar(car1);
         factory.buildCar(car2);
+	  factory.buildCar(car2);	
     }
 }
