913c2e209381d6cae4f70c8c0323522cbda939dc
Helene Christ <s1hechri@uni-trier.de>
2023-11-09 15:17:08 +0100

    Änderung für Mergekonflikt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
         Car car1 = new Car("red", "BMW", 5);
         Car car2 = new Car("blue", "Audi", 3);
         Car car3 = new Car("blue", "VW", 3);
+        Car car4 = new Car("yellow", "Renault", 4);
 
         factory.buildCar(car1);
         factory.buildCar(car2);
