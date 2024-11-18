d303f538ac290c2c07dfb0467f62ebb76f0541a7
Nick <xieagle@web.de>
2024-11-18 11:45:40 +0100

    Änderung mergeconflict

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Factory factory = new Factory();
 
-        Car car1 = new Car("red", "BMW", 5);
+        Car car1 = new Car("red", "BMW", 777);
         Car car2 = new Car("blue", "Audi", 3);
         Car car3 = new Car("blue", "VW", 3);
 
