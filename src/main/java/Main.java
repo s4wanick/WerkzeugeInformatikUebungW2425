0939bf108c362523642939cca1f4bdb23f2bab54
Nick <xieagle@web.de>
2024-11-18 11:52:05 +0100

    Konfliktlösung

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,10 @@ public class Main {
     public static void main(String[] args) {
         Factory factory = new Factory();
 
-<<<<<<< HEAD
+
         Car car1 = new Car("red", "BMW", 77);
         Car car2 = new Car("green", "Audi", 3);
-=======
-        Car car1 = new Car("red", "BMW", 77);
-        Car car2 = new Car("blue", "Audi", 3);
->>>>>>> merge-conflict
+
         Car car3 = new Car("blue", "VW", 3);
 		Car car4 = new Car("yellow", "Renault", 4);
       
